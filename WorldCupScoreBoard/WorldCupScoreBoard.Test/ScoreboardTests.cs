39f4cb10bb18058c28b2e4238dd2e348bc7dfec1
Artyom Avetisyan <artyom.avetisyan@gmail.com>
2024-02-04 18:40:48 +0100

    Adding a match to the scoreboard is tested

--- WorldCupScoreBoard/WorldCupScoreBoard.Test/ScoreboardTests.cs
+++ WorldCupScoreBoard/WorldCupScoreBoard.Test/ScoreboardTests.cs
@@ -11,5 +11,19 @@ namespace WorldCupScoreBoard.Test
 
             scoreboard.GetMatchesSummary().Should().BeEmpty();
         }
+
+        [Fact]
+        public void Scoreboard_Should_Contain_Matches_When_Added()
+        {
+            var scoreboard = new Scoreboard();
+            var homeTeam = new Team("Germany");
+            var awayTeam = new Team("Brazil");
+            var startTime = new DateTime(2024, 1, 1);
+
+            var match = new Match(homeTeam, awayTeam, startTime);
+            scoreboard.AddMatch(match);
+
+            scoreboard.GetMatchesSummary().Should().HaveCount(1);
+        }
     }
 }
