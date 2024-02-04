7bec1866472a86086af0c4b3be723d3f0e853d8e
Artyom Avetisyan <artyom.avetisyan@gmail.com>
2024-02-04 15:58:59 +0100

    Match initial score is set to 0 - 0

--- WorldCupScoreBoard/WorldCupScoreBoard/Match.cs
+++ WorldCupScoreBoard/WorldCupScoreBoard/Match.cs
@@ -6,10 +6,17 @@
         {
             HomeTeam = homeTeam;
             AwayTeam = awayTeam;
+
+            HomeTeamScore = 0;
+            AwayTeamScore = 0;
         }
 
         public Team HomeTeam { get; }
         
         public Team AwayTeam { get; }
+
+        public int HomeTeamScore { get; private set; }
+
+        public int AwayTeamScore { get; private set; }
     }
 }
