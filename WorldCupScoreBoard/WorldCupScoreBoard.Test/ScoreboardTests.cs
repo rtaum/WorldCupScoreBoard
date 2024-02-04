8eec18314befc9022afe343602bc3ab24857bd94
Artyom Avetisyan <artyom.avetisyan@gmail.com>
2024-02-04 18:27:08 +0100

    Scoreboard empty test is added

---
+++ WorldCupScoreBoard/WorldCupScoreBoard.Test/ScoreboardTests.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+
+namespace WorldCupScoreBoard.Test
+{
+    public class ScoreboardTests
+    {
+        [Fact]
+        public void Scoreboard_Should_Be_Empty()
+        {
+            var scoreboard = new Scoreboard();
+
+            scoreboard.Matches.Should().BeEmpty();
+        }
+    }
+}
