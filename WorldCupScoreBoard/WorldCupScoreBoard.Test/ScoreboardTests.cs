1091016e6dcbbf38bb0e2815a622b52240ca0bc7
Artyom Avetisyan <artyom.avetisyan@gmail.com>
2024-02-04 18:49:00 +0100

    Duplicate match test is added

--- WorldCupScoreBoard/WorldCupScoreBoard.Test/ScoreboardTests.cs
+++ WorldCupScoreBoard/WorldCupScoreBoard.Test/ScoreboardTests.cs
@@ -16,14 +16,33 @@ namespace WorldCupScoreBoard.Test
         public void Scoreboard_Should_Contain_Matches_When_Added()
         {
             var scoreboard = new Scoreboard();
+            Match match = BuildMatch();
+            scoreboard.AddMatch(match);
+
+            scoreboard.GetMatchesSummary().Should().HaveCount(1);
+        }
+
+        [Fact]
+        public void Scoreboard_Should_Not_Allow_Duplicate_Matches()
+        {
+            var scoreboard = new Scoreboard();
+            Match match = BuildMatch();
+            scoreboard.AddMatch(match);
+
+            Action act = () => scoreboard.AddMatch(match);
+
+            act.Should().Throw<ArgumentException>()
+                .WithMessage("Scoreboard already contains a match with the same teams");
+        }
+
+        private static Match BuildMatch()
+        {
             var homeTeam = new Team("Germany");
             var awayTeam = new Team("Brazil");
             var startTime = new DateTime(2024, 1, 1);
 
             var match = new Match(homeTeam, awayTeam, startTime);
-            scoreboard.AddMatch(match);
-
-            scoreboard.GetMatchesSummary().Should().HaveCount(1);
+            return match;
         }
     }
 }
